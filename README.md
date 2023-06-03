633241370222813eae170dc3f787c87d56ab312d
bapnator <80183828+bapnator@users.noreply.github.com>
2023-06-03 02:00:41 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# Quieressermi.github.io
+
+Con esta pagina para mandarsela a esa persona especial siempre caera rendida con el encanto de esta pagina
+
+Puedes hacer un git clone para poder modificarla simplemente activala en un repositorio de github pages y así modificar el archivo html y css
+
+
+Que la fuerza te acompañe
---
+++ app.js
@@ -0,0 +1,20 @@
+
+let boton = document.querySelector(".reproductor")
+
+boton.addEventListener("click", () => {
+let etiquetaAudio = document.createElement("audio")
+etiquetaAudio.setAttribute("src", "song.mp3")
+etiquetaAudio.play()
+})
+
+function mueveElBoton() {
+    width = window.innerWidth;
+    height = window.innerHeight;
+
+    newWidth = (Math.random() * width);
+    newHeight = (Math.random() * height);
+
+    document.getElementById('btnNo').style.position = "absolute";
+    document.getElementById('btnNo').style.left = newWidth + "px";
+    document.getElementById('btnNo').style.top = newHeight + "px";
+}
